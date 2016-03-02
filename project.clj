bab9b870fedb9903928dd7fdfb3aad755eb22080
Ben Siver <bsiver@gmail.com>
2016-03-02 14:50:59 -0600

    Updated project.clj to reflect fork

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject io.forward/sendgrid-clj "1.0"
-  :description "Sendgrid API wrapper"
-  :url "http://owainlewis.com"
+(defproject com.bsiver/sendgrid-clj "1.0"
+  :description "Fork of Sendgrid API wrapper"
+  :url "http://github.com/bsiver/sendgrid-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
